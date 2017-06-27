39e27ad9cafa73809d6ac761d7e441ba7c0d0c80
Mitko Stoikov <mitkostoikov1988@gmail.com>
2017-06-27 17:28:38 +0300

    update/ added pug-bootstrap

--- team-project/app/controllers/home.js
+++ team-project/app/controllers/home.js
@@ -1,6 +1,7 @@
-let express = require('express'),
-  router = express.Router(),
-  Article = require('../models/article');
+const express = require('express');
+const router = express.Router();
+
+const Article = require('../models/article');
 
 module.exports = function(app) {
   app.use('/', router);
@@ -8,8 +9,8 @@ module.exports = function(app) {
 
 router.get('/', function(req, res, next) {
   const articles = [new Article(), new Article()];
-    res.render('index', {
-      title: 'Generator-Express MVC',
-      articles: articles,
-    });
+  res.render('index', {
+    title: 'Generator-Express MVC',
+    articles: articles,
+  });
 });
