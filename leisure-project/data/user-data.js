9063a2f1d46595fd4c62bf5b31b533f5b62b2a65
Mitko Stoikov <mitkostoikov1988@gmail.com>
2017-07-06 00:05:03 +0300

    added some database functions for actions with users

--- leisure-project/data/user-data.js
+++ leisure-project/data/user-data.js
@@ -1,3 +1,7 @@
+const assert = require('assert');
+
+// Assert actions should be in a separate validator class
+// Check out validator npm also -> good way to validate stuff
 module.exports = (usersCollection) => {
     return {
         getAllUsers() {
@@ -5,6 +9,29 @@ module.exports = (usersCollection) => {
                 usersCollection.find({})
                     .then((users) => {
                         resolve(users);
+                    })
+                    .catch((err) => {
+                        console.log(err);
+                    });
+            });
+        },
+        getUserById(idObj) {
+            return new Promise((resolve, reject) => {
+                usersCollection.findOne(idObj)
+                    .then((user) => {
+                        resolve(user);
+                    });
+            });
+        },
+        createUser(userObject) {
+            return new Promise((resolve, reject) => {
+                usersCollection.insertOne(userObject)
+                    .then((response, err) => {
+                        assert.equal(null, err);
+                        // Not tested ... params may be the other way around
+                        assert.equal(1, response.insertedCount);
+
+                        resolve();
                     });
             });
         },
