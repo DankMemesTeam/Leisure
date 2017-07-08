02990a9bccd5015ada4d437151b7685ded4da58b
vixataaa <vmetodiev93@gmail.com>
2017-07-08 14:56:28 +0300

    Integrate user login/register with bcrypt, remove password-hash and validator packages

--- leisure-project/lib/controllers/auth-controller.js
+++ leisure-project/lib/controllers/auth-controller.js
@@ -1,6 +1,8 @@
 const bcrypt = require('bcrypt');
 
 module.exports = ({ userData }) => {
+    const hashingSaltRounds = 10;
+
     return {
         loadLoginPage(req, res) {
             res.render('login');
@@ -11,16 +13,20 @@ module.exports = ({ userData }) => {
         registerUser(req, res) {
             const user = req.body;
 
-            user.hashedPassword = 'muchHash';
+            bcrypt.hash(user.password, hashingSaltRounds)
+                .then((hashedPassword) => {
+                    user.hashedPassword = hashedPassword;
 
-            userData.createUser(user)
-                .then(res.redirect('/'));
+                    userData.createUser(user)
+                        .then(res.redirect('/'));
+                });
+
         },
         getUser(req, res) {
             userData.getAllUsers()
-            .then((users) => {
-                console.log(users);
-            });
+                .then((users) => {
+                    console.log(users);
+                });
         },
     };
 };
