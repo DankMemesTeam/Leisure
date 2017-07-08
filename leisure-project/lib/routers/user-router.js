239c2e248ebc8864443a715e101ba6944e125262
Mitko Stoikov <mitkostoikov1988@gmail.com>
2017-07-08 19:07:41 +0300

    update / profile page owner validation

--- leisure-project/lib/routers/user-router.js
+++ leisure-project/lib/routers/user-router.js
@@ -2,11 +2,9 @@ module.exports = ({ app, express, controllers }) => {
     const router = new express.Router();
     const { userController } = controllers;
 
-    router.get('/:username/settings', userController.loadProfileSettingsPage);
-
     router.get('/:username', userController.loadProfilePage);
     router.post('/:username/posts', userController.insertPost);
-
+    router.get('/:username/settings', userController.loadProfileSettingsPage);
 
     app.use('/user', router);
 };
