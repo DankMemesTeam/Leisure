5315079f77c6887da231f9d169c3523eae84cb06
Mitko Stoikov <mitkostoikov1988@gmail.com>
2017-07-24 14:15:40 +0300

    update

--- leisure-project/lib/controllers/home-controller.js
+++ leisure-project/lib/controllers/home-controller.js
@@ -1,8 +1,6 @@
 module.exports = ({}) => {
   return {
     loadHomePage(req, res) {
-      console.log('eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee');
-      console.log(req.user);
       res.render('home', {
         title: 'Generator-Express MVC',
       });
--- leisure-project/public/js/user-profile.js
+++ leisure-project/public/js/user-profile.js
@@ -1,5 +1,4 @@
 /* globals $ */
-$('.button-collapse').sideNav();
 
 $(() => {
     const maxCharacters = 200;
