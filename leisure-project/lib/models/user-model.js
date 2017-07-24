6d4c9ce3359b00875eb1a6dcb0a08add94ce5fff
Mitko Stoikov <mitkostoikov1988@gmail.com>
2017-07-24 20:13:40 +0300

    update

--- leisure-project/lib/models/user-model.js
+++ leisure-project/lib/models/user-model.js
@@ -3,11 +3,12 @@ const config = require('../../config/config.js').prod;
 module.exports = class User {
     constructor(username, firstName, lastName, email, hashedPassword) {
         this.username = username;
-        this.dateJoined = new Date();
-        this.profilePic = config.defaultProfilePic;
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
         this.hashedPassword = hashedPassword;
+        this.profilePic = config.defaultProfilePic;
+        this.dateJoined = new Date();
+        this.followers = [];
     }
 };
