32e7c668d265f3faa79098712cc5de892125dae9
vixataaa <vmetodiev93@gmail.com>
2017-07-31 22:46:30 +0300

    Update config

--- leisure-project/config/app-conf.js
+++ leisure-project/config/app-conf.js
@@ -2,7 +2,7 @@ module.exports = (logger) => {
     const validator = require('validator');
 
     const config = require('./config');
-    const app = require('./express-conf')(config.dev, logger);
+    const app = require('./express-conf')(config, logger);
     const express = require('express');
     const validators = require('../validators')(validator);
     const hashGenerator = require('../bin/hash-generator');
@@ -10,11 +10,11 @@ module.exports = (logger) => {
     const dataConfig = require('./data-conf');
 
 
-    return require('../database').connection(config.dev.connectionString)
+    return require('../database').connection(config.connectionString)
         .then((db) => {
             const data = require('../data')(db, validators, loadedModels, logger, dataConfig);
             const controllers = require('../lib/controllers')(data, hashGenerator, validators);
-            const application = require('./auth-conf')(app, data, db, config.dev.secretString, hashGenerator);
+            const application = require('./auth-conf')(app, data, db, config.secretString, hashGenerator);
             const routes = require('../lib/routers')(application, express, controllers);
             const server = require('./socket-conf')(application, data, controllers.chatController);
 
--- leisure-project/config/config.js
+++ leisure-project/config/config.js
@@ -4,38 +4,35 @@ const path = require('path');
 const rootPath = path.normalize(__dirname + '/..');
 
 // eslint-disable-next-line no-process-env
-const environment = process.env;
+const env = process.env.NODE_ENV || 'dev';
 // eslint-disable-next-line no-process-env
-const environmentPort = process.env.PORT;
+const envPort = process.env.PORT;
+// eslint-disable-next-line no-process-env
+const envConnString = process.env.MONGO;
 
 // Probably contants should be automaticaly adjusted
 // to environment ( not explicitly named .dev or .prod )
 const config = {
-  development: {
-    root: rootPath,
-    app: {
-      name: 'leisure-project',
-    },
-    port: environmentPort || 3000,
-    // connectionString: 'mongodb://localhost:27017/leisureDb',
-    connectionString: 'mongodb://52.57.81.182:27017/leisureDb',
-    secretString: 'Secret magical popcorn',
-    defaultProfilePic: '/images/default-user.png',
-  },
-  production: {
-    root: rootPath,
-    app: {
-      name: 'leisure-project',
-    },
-    port: environmentPort || 3001,
-    // Not sure about the syntax about this one
-    connectionString: environment.connectionString,
-    defaultProfilePic: '/images/default-user.png',
-  },
+	dev: {
+		root: rootPath,
+		app: {
+			name: 'leisure-project',
+		},
+		port: envPort || 3000,
+		connectionString: envConnString || 'mongodb://localhost:27017/leisureDb',
+		secretString: 'Secret magical popcorn',
+		defaultProfilePic: '/images/default-user.png',
+	},
+	production: {
+		root: rootPath,
+		app: {
+			name: 'leisure-project',
+		},
+		port: envPort,
+		connectionString: envConnString,
+		secretString: 'Secret magical popcorn',
+		defaultProfilePic: '/images/default-user.png',
+	},
 };
 
-// Should take dynamic process env
-module.exports = {
-  dev: config.development,
-  prod: config.production,
-};
+module.exports = config[env];
