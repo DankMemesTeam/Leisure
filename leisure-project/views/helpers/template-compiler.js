6c42761ded536dabbdc6c67642b10683b14cefe7
Mitko Stoikov <mitkostoikov1988@gmail.com>
2017-08-03 11:23:10 +0300

    update

--- leisure-project/views/helpers/template-compiler.js
+++ leisure-project/views/helpers/template-compiler.js
@@ -1,11 +1,14 @@
 module.exports = () => {
     const pug = require('pug');
+    const path = require('path');
 
     return {
         render(templateName, templateObj) {
             return new Promise((resolve, reject) => {
+                const filePath = path.join(__dirname, `../dynamic/${templateName}.pug`);
+
                 const compiledTemplate =
-                    pug.renderFile(`../dynamic/${templateName}.pug`,
+                    pug.renderFile(filePath,
                      templateObj);
 
                 resolve(compiledTemplate);
